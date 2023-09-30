eac98d38431c2be811ed8d3290f3f659394a24ef
Samuel Amâncio Santos <127424077+Samuel-Amancio-Santos@users.noreply.github.com>
2023-09-30 13:36:24 -0300

    commit2

--- src/components/Newslatter.js
+++ src/components/Newslatter.js
@@ -1,5 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap"
-
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
