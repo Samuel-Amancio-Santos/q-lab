c26e6001e2d88000b682b987e8ab16505e6a2220
Samuel Amâncio Santos <127424077+Samuel-Amancio-Santos@users.noreply.github.com>
2023-11-28 20:18:26 -0300

    deploy

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,1 +1,1 @@
-export const BACKEND_BASE_URL =  process.env.BACKEND_BASE_URL || "http://localhost:5000";
+export const BACKEND_BASE_URL =  process.env.REACT_APP_BACKEND_BASE_URL || "http://localhost:5000";
