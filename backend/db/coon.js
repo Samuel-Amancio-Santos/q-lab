fb832d0111bac21822db4185177f64c87371614f
vinicius-r-rosa <90712926+vinicius-r-rosa@users.noreply.github.com>
2024-02-27 16:05:23 -0300

    Removendo dado sensível do projeto

--- backend/db/coon.js
+++ backend/db/coon.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+require("dotenv").config();
+
+const DB_COON = process.env.DB_COON || "insira_a_connection_string_db"
 
 async function main(){
 
@@ -6,7 +9,7 @@ async function main(){
 
         mongoose.set("strictQuery", true);
         
-        await mongoose.connect("mongodb+srv://Pedro:nQMeNoE2PQtglE7p@cluster0.9a4zdvt.mongodb.net/?retryWrites=true&w=majority");
+        await mongoose.connect(DB_COON);
 
         console.log("Conectado ao banco!!")
 
